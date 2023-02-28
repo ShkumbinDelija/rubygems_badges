29108359afb4209b741b46c55a4171932585aba3
Shkumbin Delija <shkumbin.delija@gmail.com>
2023-02-28 14:43:47 +0100

    Add support for fetching owner gem downloads

--- web.rb
+++ web.rb
@@ -14,10 +14,25 @@ def fetch_badge(gem_downloads:)
   Net::HTTP.get_response(URI("https://img.shields.io/badge/gem%20downloads-#{gem_downloads}-green")).body
 end
 
-get '/:gem_name' do
+def fetch_owner_downloads(owner:)
+  JSON(Net::HTTP.get_response(URI("https://rubygems.org/api/v1/owners/#{owner}/gems.json")).body).sum do |gem|
+    gem['downloads']
+  end
+end
+
+get '/gems/:gem_name' do
   gem_name = params.fetch('gem_name')
 
   gem_downloads = number_with_delimiter(number: fetch_gem_downloads(gem_name: gem_name))
 
   fetch_badge(gem_downloads: gem_downloads)
 end
+
+get '/owners/:owner' do
+  owner = params.fetch('owner')
+
+  gem_downloads = number_with_delimiter(number: fetch_owner_downloads(owner: owner))
+
+  fetch_badge(gem_downloads: gem_downloads)
+end
+
