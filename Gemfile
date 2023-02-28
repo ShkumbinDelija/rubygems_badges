ad09a402340f40c75da49d337f52208644fd033f
Shkumbin Delija <shkumbin.delija@gmail.com>
2023-02-28 14:51:44 +0100

    fix

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@
 
 source "https://rubygems.org"
 
+gem "webrick"
 gem "sinatra"
--- web.rb
+++ web.rb
@@ -1,38 +1,39 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'uri'
 require 'net/http'
 
-def fetch_gem_downloads(gem_name:)
-  JSON(Net::HTTP.get_response(URI("https://rubygems.org/api/v1/gems/#{gem_name}.json")).body)['downloads']
-end
+class Web < Sinatra::Base
+  def fetch_gem_downloads(gem_name:)
+    JSON(Net::HTTP.get_response(URI("https://rubygems.org/api/v1/gems/#{gem_name}.json")).body)['downloads']
+  end
 
-def number_with_delimiter(number:)
-  number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-end
+  def number_with_delimiter(number:)
+    number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
 
-def fetch_badge(gem_downloads:)
-  Net::HTTP.get_response(URI("https://img.shields.io/badge/gem%20downloads-#{gem_downloads}-green")).body
-end
+  def fetch_badge(gem_downloads:)
+    Net::HTTP.get_response(URI("https://img.shields.io/badge/gem%20downloads-#{gem_downloads}-green")).body
+  end
 
-def fetch_owner_downloads(owner:)
-  JSON(Net::HTTP.get_response(URI("https://rubygems.org/api/v1/owners/#{owner}/gems.json")).body).sum do |gem|
-    gem['downloads']
+  def fetch_owner_downloads(owner:)
+    JSON(Net::HTTP.get_response(URI("https://rubygems.org/api/v1/owners/#{owner}/gems.json")).body).sum do |gem|
+      gem['downloads']
+    end
+  end
+
+  get '/gems/:gem_name' do
+    gem_name = params.fetch('gem_name')
+
+    gem_downloads = number_with_delimiter(number: fetch_gem_downloads(gem_name: gem_name))
+
+    fetch_badge(gem_downloads: gem_downloads)
+  end
+
+  get '/owners/:owner' do
+    owner = params.fetch('owner')
+
+    gem_downloads = number_with_delimiter(number: fetch_owner_downloads(owner: owner))
+
+    fetch_badge(gem_downloads: gem_downloads)
   end
 end
-
-get '/gems/:gem_name' do
-  gem_name = params.fetch('gem_name')
-
-  gem_downloads = number_with_delimiter(number: fetch_gem_downloads(gem_name: gem_name))
-
-  fetch_badge(gem_downloads: gem_downloads)
-end
-
-get '/owners/:owner' do
-  owner = params.fetch('owner')
-
-  gem_downloads = number_with_delimiter(number: fetch_owner_downloads(owner: owner))
-
-  fetch_badge(gem_downloads: gem_downloads)
-end
-
